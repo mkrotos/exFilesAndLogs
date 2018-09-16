e17536d2914407ce7736f1faf1c7d16bb6c96d87
Mariusz Krotos <mar.krotos@gmail.com>
2018-09-16 11:04:06 +0200

    exercise for files

---
+++ src/main/java/com/krotos/FilesBasics.java
@@ -0,0 +1,66 @@
+package com.krotos;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilesBasics {
+
+    public static void main(String[] args) throws IOException {
+
+        //printListOfFiles();
+        //printDetails();
+        readFile();
+        //writeFile();
+        findFiles();
+    }
+
+    private static void findFiles() throws IOException {
+        String fileName="D:\\kody\\Java\\Exercises\\exFilesLogs\\";
+        Path dir=Paths.get(fileName);
+        int maxDepth=5;
+
+        Files.walk(dir,maxDepth).forEach(System.out::println);
+
+    }
+
+    private static void printDetails() throws IOException {
+        String fileName="C:\\swapfile.sys";
+        Path path=Paths.get(fileName);
+
+        BasicFileAttributes attr= java.nio.file.Files.readAttributes(path,BasicFileAttributes.class);
+
+        System.out.println("create time: "+attr.creationTime());
+        System.out.println("last access time: "+attr.lastAccessTime());
+        System.out.println("size: "+attr.size()/1024/1024);
+        System.out.println("last modify time: "+attr.lastModifiedTime());
+        System.out.println("is directory: "+attr.isDirectory());
+    }
+
+    private static void printListOfFiles() throws IOException {
+        String startPath="C:/";
+        Path dir = Paths.get(startPath);
+        java.nio.file.Files.list(dir)
+                .forEach(System.out::println);
+    }
+
+    private static void readFile() throws IOException {
+        String fileName="D:\\kody\\Java\\Exercises\\exFilesLogs\\tekst.txt";
+        Path dir=Paths.get(fileName);
+        java.nio.file.Files.lines(dir)
+                .forEach(System.out::println);
+    }
+
+    private static void writeFile() throws IOException {
+        String fileName="C:\\Users\\makro\\Desktop\\tmp.txt";
+        Path path=Paths.get(fileName);
+        List<String> linesToWrite= Arrays.asList("Linia 1","Linia 2", "Linia 3");
+
+        Files.write(path,linesToWrite);
+    }
+}
--- src/main/java/com/krotos/FilesEx.java
+++ src/main/java/com/krotos/FilesEx.java
@@ -1,66 +1,56 @@
 package com.krotos;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FilesEx {
 
     public static void main(String[] args) throws IOException {
-
-        //printListOfFiles();
-        //printDetails();
-        readFile();
-        //writeFile();
-        findFiles();
+        findTxtAndWriteFirstLines();
     }
 
-    private static void findFiles() throws IOException {
-        String fileName="D:\\kody\\Java\\Exercises\\exFilesLogs\\";
-        Path dir=Paths.get(fileName);
-        int maxDepth=5;
 
-        Files.walk(dir,maxDepth).forEach(System.out::println);
+    private static void findTxtAndWriteFirstLines() throws IOException {
+        String pathString = "C:/totalcmd";
+        Path path = Paths.get(pathString);
+        int depth = 5;
+        //Files.walk(path, depth).forEach(System.out::println);
+        String pathToNewFile = "ex1.txt";
+        Path newPath = Paths.get(pathToNewFile);
+        if (!Files.exists(newPath)) {
+            Files.createFile(newPath);
+        }
 
-    }
+        Map<String, String> mapOfLines = new HashMap<>();
 
-    private static void printDetails() throws IOException {
-        String fileName="C:\\swapfile.sys";
-        Path path=Paths.get(fileName);
+        List<Path> paths = Files.walk(path, depth)
+                .filter(file -> file.toString().endsWith(".TXT") || file.toString().endsWith(".txt"))
+                .collect(Collectors.toList());
 
-        BasicFileAttributes attr= java.nio.file.Files.readAttributes(path,BasicFileAttributes.class);
+        //System.out.println(paths);
 
-        System.out.println("create time: "+attr.creationTime());
-        System.out.println("last access time: "+attr.lastAccessTime());
-        System.out.println("size: "+attr.size()/1024/1024);
-        System.out.println("last modify time: "+attr.lastModifiedTime());
-        System.out.println("is directory: "+attr.isDirectory());
-    }
+        for (Path path1 : paths) {
+            String name = path1.getFileName().toString();
+            String line = Files.lines(path1).findFirst().get();
+            mapOfLines.put(name, line);
+        }
+        //System.out.println(mapOfLines);
+        List<String> listOfEntryToFile = new ArrayList<>();
 
-    private static void printListOfFiles() throws IOException {
-        String startPath="C:/";
-        Path dir = Paths.get(startPath);
-        java.nio.file.Files.list(dir)
-                .forEach(System.out::println);
-    }
+        for (Map.Entry entry : mapOfLines.entrySet()) {
+            String line = entry.getKey().toString()+" : "+entry.getValue().toString();
+            listOfEntryToFile.add(line);
 
-    private static void readFile() throws IOException {
-        String fileName="D:\\kody\\Java\\Exercises\\exFilesLogs\\tekst.txt";
-        Path dir=Paths.get(fileName);
-        java.nio.file.Files.lines(dir)
-                .forEach(System.out::println);
-    }
+        }
+        Files.write(newPath, listOfEntryToFile);
 
-    private static void writeFile() throws IOException {
-        String fileName="C:\\Users\\makro\\Desktop\\tmp.txt";
-        Path path=Paths.get(fileName);
-        List<String> linesToWrite= Arrays.asList("Linia 1","Linia 2", "Linia 3");
 
-        Files.write(path,linesToWrite);
     }
 }
