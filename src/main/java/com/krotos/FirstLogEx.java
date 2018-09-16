ace1ed69e27b527d70e2e4142602f2d9b32e40f0
Mariusz Krotos <mar.krotos@gmail.com>
2018-09-16 14:29:07 +0200

    log exercise ver 2

--- src/main/java/com/krotos/FirstLogEx.java
+++ src/main/java/com/krotos/FirstLogEx.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,6 +16,9 @@ public class FirstLogEx {
 
     public static void main(String[] args) throws IOException {
         dispCountsOfFileFormats();
+        System.out.println("---------------------------");
+        dispCountsVer2();
+
     }
 
     private static void dispCountsOfFileFormats() throws IOException {
@@ -28,33 +32,42 @@ public class FirstLogEx {
 
         for (String extension : uniqueExtensions) {
             long numberOfInstances = Files.walk(pathForFiles).filter(pathsWithSpecifiedExtension(extension)).count();
-            String lineToDisplay = String.format("%s - %s", extension, numberOfInstances);
-            System.out.println(lineToDisplay);
+            dispLineOfStringAndLong(extension, numberOfInstances);
         }
+    }
+
+    private static void dispCountsVer2() throws IOException {
+        String pathString = "C:/totalcmd";
+        Path pathForFiles = Paths.get(pathString);
+        int depth = 10;
+        //Files.walk(pathForFiles, depth).forEach(System.out::println);
+
+        List<String> nonUniqExtensions=Files.walk(pathForFiles, depth).filter(Files::isRegularFile).map(FirstLogEx::getExtension).collect(Collectors.toList());
+        Map<String,Long> results= nonUniqExtensions.stream().collect(Collectors.groupingBy(s->s,Collectors.counting()));
+
+        for (Map.Entry entry:results.entrySet()){
+            dispLineOfStringAndLong(entry.getKey().toString(), (long) entry.getValue());
+        }
+    }
+
+    private static void dispLineOfStringAndLong(String extension, long numberOfInstances) {
+        String lineToDisplay = String.format("%s - %s", extension, numberOfInstances);
+        System.out.println(lineToDisplay);
     }
 
     private static Predicate<Path> pathsWithSpecifiedExtension(String extension) {
         return path -> path.getFileName().toString().endsWith("." + extension);
     }
 
-    private static Predicate<Path> onlyFiles() {
-        return path -> {
-            try {
-                return Files.readAttributes(path, BasicFileAttributes.class).isRegularFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-        };
-    }
-
     private static String getExtension(Path path) {
         try {
             String[] splittedPath = path.toString().split("\\.");
+
             int lastElement = splittedPath.length - 1;
             if(lastElement==0){
                 return WITHOUT_EXTENSION_DEFAULT;
             }
+
             return splittedPath[lastElement];
         } catch (ArrayIndexOutOfBoundsException e) {
             return WITHOUT_EXTENSION_DEFAULT;
